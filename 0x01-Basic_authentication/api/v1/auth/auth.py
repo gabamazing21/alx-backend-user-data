96a6b7e3a3c6ef26abba51cdd97c68906c18c082
gabriel <gabaamazing21@gmail.com>
2024-11-15 00:59:23 +0100

    require_auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from flask import request
 from typing import List, TypeVar
 from models.user import User
@@ -5,22 +6,34 @@ from models.user import User
 This module deals with api authentication
 """
 
+
 class Auth():
     """
     this class enable auth method
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-        False - path 
+        False - path
         """
-        return False 
-    
+        if path is None:
+            return True
+
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        normalized_path = path.rstrip('/')
+
+        for exclude_path in excluded_paths:
+            if normalized_path == exclude_path.rstrip('/'):
+                return False
+            return True
+
     def authorization_header(self, request=None) -> str:
         """
         returns None - request will be the Flask request object
         """
         return request
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """
          that returns None - request will be the Flask request object
